6207ff75dad5dbb08eeafc17c6cdda5cb947b041
lyriq-cloudsolutions <lyriq@geniouscloudsolutions.com>
2025-04-15 14:18:04 -0400

    added subnet ids

--- modules/three-tier/outputs.tf
+++ modules/three-tier/outputs.tf
@@ -8,4 +8,28 @@ output "backend_instance_id" {
 
 output "frontend_instance_id" {
   value = module.Frontend.ec2_instance_id
-} 
+}
+
+output "vpc_id" {
+  value = aws_vpc.ThreeTierAppVPC.id
+}
+
+output "vpc_arn" {
+  value = aws_vpc.ThreeTierAppVPC.arn
+}
+
+output "publicsubnet1_id" {
+  value = aws_subnet.PublicSubnet1.id
+}
+
+output "publicsubnet1_arn" {
+  value = aws_subnet.PublicSubnet1.arn
+}
+
+output "publicsubnet2_id" {
+  value = aws_subnet.PublicSubnet2.id
+}
+
+output "publicsubnet2_arn" {
+  value = aws_subnet.PublicSubnet2.arn
+}
