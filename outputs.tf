7f97d928c491c46be88306653371c8dae4873c55
lyriq-cloudsolutions <lyriq@geniouscloudsolutions.com>
2025-04-26 11:21:23 -0400

    ami demo

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-output "vpc_id" {
-  value = module.three_tier.vpc_id
-}
+# output "vpc_id" {
+#   value = module.three_tier.vpc_id
+# }
 
-output "publicsubnet1_id" {
-  value = module.three_tier.publicsubnet1_id
-}
+# output "publicsubnet1_id" {
+#   value = module.three_tier.publicsubnet1_id
+# }
 
-output "publicsubnet2_id" {
-  value = module.three_tier.publicsubnet2_id
-}
+# output "publicsubnet2_id" {
+#   value = module.three_tier.publicsubnet2_id
+# }
