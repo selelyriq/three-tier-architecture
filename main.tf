41acd0f3f0a41c5f52b6c99f7aab77a1b9d5f86b
lyriq-cloudsolutions <lyriq@geniouscloudsolutions.com>
2025-04-26 22:36:42 -0400

    no keypair

--- main.tf
+++ main.tf
@@ -15,7 +15,6 @@ data "aws_ami" "my_ami" {
 resource "aws_instance" "ec2_instance" {
   ami                    = data.aws_ami.my_ami.id
   instance_type          = "t2.micro"
-  key_name               = "EC2"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.allow_https.id]
   subnet_id              = aws_subnet.public_subnet.id
   iam_instance_profile   = data.aws_iam_instance_profile.demo_instance_profile.name
